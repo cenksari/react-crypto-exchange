fba0af2fcdcb003f4c98aca0610f9c9354e66d96
Cenk SARI <cenk@cenksari.com>
2021-09-17 18:18:29 +0300

    new screens and widgets

--- src/screens/Capital/CapitalScreen.js
+++ src/screens/Capital/CapitalScreen.js
@@ -1,9 +1,80 @@
+import { Sparklines, SparklinesLine } from 'react-sparklines';
+
 import SiteLayout from '../../layouts/SiteLayout';
 import Header from '../../components/Header/Header';
 
 const CapitalScreen = () => (
   <SiteLayout>
     <Header icon='sort' title='Market capital' />
+    <div className='top-buttons flex flex-center flex-space-between'>
+      <div>
+        <div className='top-search'>
+          <input type='text' id='search' name='search' placeholder='Aranacak kelime' />
+          <button type='button' className='pointer'>
+            <i className='material-icons'>search</i>
+          </button>
+        </div>
+      </div>
+      <div>
+        <button type='button' className='button button-white button-large'>
+          <i className='material-icons button-icon-left'>event</i>
+          Periyod
+          <i className='material-icons button-icon-right'>keyboard_arrow_down</i>
+        </button>
+        <button type='button' className='button button-purple button-large'>
+          <i className='material-icons button-icon-left'>download</i>
+          CSV indir
+        </button>
+      </div>
+    </div>
+
+    <table className='data-table'>
+      <thead>
+        <tr>
+          <th className='left'>Rank</th>
+          <th className='left'>Coin</th>
+          <th className='center'>Last price</th>
+          <th className='center'>Change (24h)</th>
+          <th className='center'>Volume (24h)</th>
+          <th className='left'>Graph</th>
+          <th className='right'>&nbsp;</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <div className='rank accent'>#1</div>
+          </td>
+          <td className='nowrap'>
+            <div className='icon' />
+            <strong>Bitcoin</strong>
+          </td>
+          <td className='center'>
+            <strong>$11.854.12</strong>
+          </td>
+          <td className='center'>
+            <strong className='green'>2.54%</strong>
+          </td>
+          <td className='center'>$220.080.878.451</td>
+          <td>
+            <div className='line-chart'>
+              <Sparklines
+                data={[5, 10, 5, 20, 8, 15, 22, 8, 12, 8, 32, 16, 29, 20, 16, 30, 42, 45]}
+                width={200}
+                height={50}
+              >
+                <SparklinesLine style={{ strokeWidth: 4 }} color='#32b778' />
+              </Sparklines>
+            </div>
+          </td>
+          <td className='right'>
+            <button type='button' className='pointer'>
+              <i className='material-icons'>more_vert</i>
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </SiteLayout>
 );
 
--- src/screens/Transactions/TransactionsScreen.js
+++ src/screens/Transactions/TransactionsScreen.js
@@ -3,7 +3,69 @@ import Header from '../../components/Header/Header';
 
 const TransactionsScreen = () => (
   <SiteLayout>
-    <Header icon='sort' title='Transactions' />
+    <Header icon='sort' title='Market capital' />
+    <div className='top-buttons flex flex-center flex-space-between'>
+      <div>
+        <div className='top-search'>
+          <input type='text' id='search' name='search' placeholder='Aranacak kelime' />
+          <button type='button' className='pointer'>
+            <i className='material-icons'>search</i>
+          </button>
+        </div>
+      </div>
+      <div>
+        <button type='button' className='button button-white button-large'>
+          <i className='material-icons button-icon-left'>event</i>
+          Periyod
+          <i className='material-icons button-icon-right'>keyboard_arrow_down</i>
+        </button>
+        <button type='button' className='button button-purple button-large'>
+          <i className='material-icons button-icon-left'>download</i>
+          CSV indir
+        </button>
+      </div>
+    </div>
+
+    <table className='data-table'>
+      <thead>
+        <tr>
+          <th className='left'>&nbsp;</th>
+          <th className='left'>Transaction ID</th>
+          <th className='left'>Date</th>
+          <th className='left'>From</th>
+          <th className='left'>To</th>
+          <th className='left'>Coin</th>
+          <th className='center'>Amount</th>
+          <th className='center'>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <div className='operation green'>
+              <i className='material-icons'>arrow_upward</i>
+            </div>
+          </td>
+          <td>#12415346563475</td>
+          <td>2/5/2020 06:24:45</td>
+          <td>Thomas</td>
+          <td className='nowrap'>
+            <div className='icon' />
+            Ashlynn
+          </td>
+          <td className='nowrap'>
+            <div className='icon' />
+            <strong>Bitcoin</strong>
+          </td>
+          <td className='center'>
+            <strong className='green'>+$5.553</strong>
+          </td>
+          <td className='center'>
+            <span className='status green'>PENDING</span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </SiteLayout>
 );
 
