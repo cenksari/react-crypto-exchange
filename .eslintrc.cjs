5672416fc655d4eb0c92566776cca0f4b9cb5da4
Cenk SARI <cenk@cenksari.com>
2024-08-02 10:55:31 +0100

    update packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,9 +17,11 @@ module.exports = {
   rules: {
     'prettier/prettier': 'error',
     'react/require-default-props': 'off',
+    '@typescript-eslint/no-throw-literal': 'off',
     'jsx-a11y/control-has-associated-label': 'off',
     'jsx-a11y/label-has-associated-control': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
+    '@typescript-eslint/lines-between-class-members': 'off',
     'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
   },
 };
