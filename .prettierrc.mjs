0e515a0b021c5b44e065079cae6bc1267bf08cb0
Cenk SARI <cenk@cenksari.com>
2025-04-06 10:43:41 +0100

    chore: update dependencies and configuration
    - Updated `react-router-dom` from `^7.4.1` to `^7.5.0`.
    - Updated `typescript` from `^5.8.2` to `^5.8.3`.
    - Updated dev dependencies:
      - `@types/node` from `^22.13.14` to `^22.14.0`.
      - `@types/react` from `^19.0.12` to `^19.1.0`.
      - `@types/react-dom` from `^19.0.4` to `^19.1.1`.
      - `@typescript-eslint/eslint-plugin` from `^8.28.0` to `^8.29.0`.
      - `@typescript-eslint/parser` from `^8.28.0` to `^8.29.0`.
      - `eslint` from `^9.23.0` to `^9.24.0`.
      - `eslint-plugin-prettier` from `^5.2.5` to `^5.2.6`.
      - `eslint-plugin-react` from `^7.37.4` to `^7.37.5`.
      - `typescript-eslint` from `^8.28.0` to `^8.29.0`.
    - Updated `.prettierrc.mjs` configuration by removing unused options.
    - Updated `yarn.lock` to reflect the changes in dependencies.

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,13 +1,8 @@
 const config = {
-  semi: true,
-  tabWidth: 2,
-  useTabs: false,
   printWidth: 100,
   endOfLine: 'auto',
   singleQuote: true,
   trailingComma: 'es5',
-  arrowParens: 'always',
-  bracketSpacing: true,
   jsxSingleQuote: true,
 };
 
